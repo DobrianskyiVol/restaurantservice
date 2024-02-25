c2be303f2732594b8795d534d654661b89dd1c87
DobrianskyiVol <dobrianskyi.volodymyr@chnu.edu.ua>
2024-02-25 12:28:45 +0300

    Add class Visitor

---
+++ Visitor.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 25.02.2024.
+//
+
+#ifndef RESTAURANTSERVICE_VISITOR_H
+#define RESTAURANTSERVICE_VISITOR_H
+
+
+class Visitor {
+
+};
+
+
+#endif //RESTAURANTSERVICE_VISITOR_H
